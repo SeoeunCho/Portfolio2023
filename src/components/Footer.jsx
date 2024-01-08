d7e52696f668aef20ff6749c97ee01ecd5f190df
seoeuncho <westsilver24@naver.com>
2024-01-08 13:14:54 +0900

    ➕ 푸터 수정

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import profileImg from "../assets/img/seoeuncho.png";
-import blueStarImg from "../assets/img/blue-star.png";
 
 const Footer = () => {
   return (
@@ -8,21 +6,42 @@ const Footer = () => {
       <div className="container">
         <div className="footer__inner">
           <div className="footer_icon">
-            <a href="https://github.com/SeoeunCho" className="github" target="_blank" rel="noreferrer">
+            <a
+              href="https://github.com/SeoeunCho"
+              className="github"
+              target="_blank"
+              rel="noreferrer"
+            >
               <span className="visually-hidden">github</span>
             </a>
             <a
               href="https://enchanting-drip-66d.notion.site/Secho-661e000688d04abf94686ae02d8983ff?pvs=4"
               className="notion"
               target="_blank"
-              rel="noreferrer">
+              rel="noreferrer"
+            >
               <span className="visually-hidden">notion</span>
             </a>
-            <a href="https://open.kakao.com/o/sTuSneIf" className="kakao" target="_blank" rel="noreferrer">
+            <a
+              href="https://westsilver.gitbook.io/study-script/"
+              className="gitbook"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <span className="visually-hidden">gitbook</span>
+            </a>
+            <a
+              href="https://open.kakao.com/o/sTuSneIf"
+              className="kakao"
+              target="_blank"
+              rel="noreferrer"
+            >
               <span className="visually-hidden">kakao</span>
             </a>
           </div>
-          <div className="footer_copyright">© 2023. Cho Seo Eun. All rights reserved.</div>
+          <div className="footer_copyright">
+            © 2023. Cho Seo Eun. All rights reserved.
+          </div>
         </div>
       </div>
     </footer>
