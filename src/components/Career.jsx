11ff2bfebf7b62e0e8383a4c045247d95f3de54c
seoeuncho <westsilver24@naver.com>
2024-01-31 16:44:53 +0900

    ➕ 커리어 섹션 수정

--- src/components/Career.jsx
+++ src/components/Career.jsx
@@ -8,16 +8,29 @@ const Career = () => {
           <div className="docs-hero__content">
             <div className="subtitle subtitle--small subtitle--left">
               <div className="subtitle__brace">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 27 78" aria-hidden="true">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 27 78"
+                  aria-hidden="true"
+                >
                   <path
                     fill="#bbbaa6"
                     d="M26.52 77.21h-5.75c-6.83 0-12.38-5.56-12.38-12.38V48.38C8.39 43.76 4.63 40 .01 40v-4c4.62 0 8.38-3.76 8.38-8.38V12.4C8.38 5.56 13.94 0 20.77 0h5.75v4h-5.75c-4.62 0-8.38 3.76-8.38 8.38V27.6c0 4.34-2.25 8.17-5.64 10.38 3.39 2.21 5.64 6.04 5.64 10.38v16.45c0 4.62 3.76 8.38 8.38 8.38h5.75v4.02Z"
                   />
                 </svg>
               </div>
-              <p className="subtitle__label">This is a career description of projects experience as a Front-End developer.</p>
+              <p className="subtitle__label">
+                This is a career description of projects experience as a
+                Front-End developer.
+              </p>
               <div className="subtitle__brace">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 27 78" aria-hidden="true">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 27 78"
+                  aria-hidden="true"
+                >
                   <path
                     fill="#bbbaa6"
                     d="M26.52 77.21h-5.75c-6.83 0-12.38-5.56-12.38-12.38V48.38C8.39 43.76 4.63 40 .01 40v-4c4.62 0 8.38-3.76 8.38-8.38V12.4C8.38 5.56 13.94 0 20.77 0h5.75v4h-5.75c-4.62 0-8.38 3.76-8.38 8.38V27.6c0 4.34-2.25 8.17-5.64 10.38 3.39 2.21 5.64 6.04 5.64 10.38v16.45c0 4.62 3.76 8.38 8.38 8.38h5.75v4.02Z"
@@ -29,10 +42,17 @@ const Career = () => {
             <div className="career__items">
               <ul>
                 <li className="worklist__all">경력기술서</li>
-                <a href="https://enchanting-drip-66d.notion.site/9b51f1201d4f4727bedaf90ee49312f3" target="_blank" rel="noreferrer">
+                <a
+                  href="https://enchanting-drip-66d.notion.site/9b51f1201d4f4727bedaf90ee49312f3"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <ul>
                     <li>Front-End 담당</li>
-                    <li>1년 3개월간 작업한 모든 프로젝트를 볼 수 있도록 한곳에 정리하였습니다.</li>
+                    <li>
+                      1년 3개월간 작업한 모든 프로젝트를 볼 수 있도록 한곳에
+                      정리하였습니다.
+                    </li>
                     <li>
                       <span>전체보기</span>
                     </li>
@@ -41,25 +61,26 @@ const Career = () => {
               </ul>
               <ul>
                 <li>
-                  ERP 골프장
-                  <br />
-                  서비스 웹
+                  골프장 ERP
                 </li>
-                <a href="https://enchanting-drip-66d.notion.site/ERP-31d935e6e26148f386223b1128b17b15" target="_blank" rel="noreferrer">
+                <a
+                  href="https://enchanting-drip-66d.notion.site/ERP-31d935e6e26148f386223b1128b17b15"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <ul>
                     <li>
                       웹 개발
-                      <br /> CRUD 구현
                       <br />
-                      공통컴포넌트 개발
+                      공통 UI 컴포넌트 개발
                     </li>
-                    <li>골프장 운영 통합솔루션으로 회원, 예약, 프론트, 권한 등 효율적인 관리 서비스를 제공하는 신규 개발</li>
+                    <li>골프장 관리 ERP 서비스 개발</li>
                     <li>
                       <span className="web">Web</span>
                       <span className="vue">Vue3</span>
                       <span className="js">JavaScript</span>
-                      <span>HTML</span>
-                      <span>Sass</span>
+                      <span>Swagger UI</span>
+                      <span>Git</span>
                     </li>
                   </ul>
                 </a>
@@ -70,45 +91,55 @@ const Career = () => {
                   <br />
                   1차 / 2차
                 </li>
-                <a href="https://enchanting-drip-66d.notion.site/1-2-0b6ade6a3255433881f0293c36362dae" target="_blank" rel="noreferrer">
+                <a
+                  href="https://enchanting-drip-66d.notion.site/1-2-0b6ade6a3255433881f0293c36362dae"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <ul>
                     <li>
                       모바일 웹 개발
                       <br />
                       어드민 웹 개발
                       <br />
-                      운명 및 유지보수
+                      운영 및 유지보수
                     </li>
-                    <li>아마추어 골프선수 모집 및 경기 주최, 참가신청을 받고 랭킹 기록을 제공하는 신규 서비스 개발</li>
+                    <li>
+                      아마추어 골퍼 회원 모집 및 골프 리그 관련 신규 서비스 개발
+                    </li>
                     <li>
                       <span className="web_app">WebApp</span>
                       <span className="vue">Vue.js</span>
                       <span className="vue">Vuex</span>
                       <span className="js">JavaScript</span>
-                      <span>HTML</span>
-                      <span>Sass</span>
+                      <span>Swagger UI</span>
+                      <span>Git</span>
                     </li>
                   </ul>
                 </a>
               </ul>
               <ul>
                 <li>디지털 매거진</li>
-                <a href="https://enchanting-drip-66d.notion.site/ee9109e5a2ac43989d9ae586f94223cd" target="_blank" rel="noreferrer">
+                <a
+                  href="https://enchanting-drip-66d.notion.site/ee9109e5a2ac43989d9ae586f94223cd"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <ul>
                     <li>
                       모바일 웹 개발
                       <br />
                       어드민 웹 개발
                       <br />
-                      운명 및 유지보수
+                      운영 및 유지보수
                     </li>
-                    <li>자회사인 골프매거진 코리아의 기사를 앱에서도 볼 수 있도록 매월호 정보를 제공하는 신규 서비스 개발</li>
+                    <li>온라인 매거진 신규 서비스 개발</li>
                     <li>
                       <span className="web_app">WebApp</span>
                       <span className="vue">Vue.js</span>
                       <span className="js">JavaScript</span>
-                      <span>HTML</span>
-                      <span>Sass</span>
+                      <span>Swagger UI</span>
+                      <span>Git</span>
                     </li>
                   </ul>
                 </a>
@@ -119,17 +150,21 @@ const Career = () => {
                   <br />
                   모집
                 </li>
-                <a href="https://enchanting-drip-66d.notion.site/d891a9b9813d4909a88ba4d18a0eed34" target="_blank" rel="noreferrer">
+                <a
+                  href="https://enchanting-drip-66d.notion.site/d891a9b9813d4909a88ba4d18a0eed34"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <ul>
                     <li>모바일 웹 개발</li>
-                    <li>특별회원 모집을 위한 설문조사형 이벤트페이지 개발</li>
+                    <li>설문조사형 특별회원 모집 이벤트 페이지 개발</li>
                     <li>
                       <span className="web_app">WebApp</span>
                       <span className="vue">Vue.js</span>
                       <span className="vue">Vuex</span>
                       <span className="js">JavaScript</span>
-                      <span>HTML</span>
-                      <span>Sass</span>
+                      <span>Swagger UI</span>
+                      <span>Git</span>
                     </li>
                   </ul>
                 </a>
